62e84a96b284f8caeb4a6abd1a71c87f95752f44
Clint Duran <clint.duran@cireson.com>
2023-08-23 22:06:01 +0800

    add animation to modal and add close button

--- src/components/shared/ImageArticle.tsx
+++ src/components/shared/ImageArticle.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import BigPic from '../../images/component-01/Image-01@2x.jpg';
-import FamilyPic from '../../images/component-01/Image-01.jpg';
+import CloseIcon from '../../images/close.svg';
 
 export interface ImageArticleProps {
     previewImage: string;
@@ -9,27 +8,34 @@ export interface ImageArticleProps {
     fullImage: string;
 }
 
-export const ImageArticle: React.FC<ImageArticleProps> = ({previewImage, alt, className, fullImage}: ImageArticleProps) => {
+export const ImageArticle: React.FC<ImageArticleProps> = ({ previewImage, alt, className, fullImage }: ImageArticleProps) => {
     const [isShown, setIsShown] = React.useState<boolean>(false);
     const [showFull, setShowFull] = React.useState<boolean>(false);
+
+    const closeModal = () => {
+        setIsShown(false);
+    }
+    const openModal = () => {
+        setIsShown(true);
+    }
+
     return (
         <>
-            <img src={previewImage} alt={alt} className={className} onClick={() => {setIsShown(true)}}/>
-            {isShown ?
-                <div className={`relative z-10`} aria-labelledby="modal-title" role="dialog" aria-modal="true">
-                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" onClick={() => {setIsShown(false)}}></div>
-
-                    <div className="fixed inset-0 z-10 overflow-y-auto" onClick={() => {setIsShown(false)}}>
-                        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0  flex-col md:flex-row">
-                            <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                                
-                                <img src={previewImage} onClick={(e) => {e.stopPropagation();}} className={`${showFull ? 'hidden': ''} w-full`}/> 
-                                <img src={fullImage} className={`${showFull ? '': 'hidden'}`} onClick={(e) => {e.stopPropagation();}} onLoad={() => {setShowFull(true)}}/>
+            <img src={previewImage} alt={alt} className={className} onClick={openModal} />
+            <div className={`${isShown ? '' : '-translate-y-full'} transition-transform duration-500  w-full h-screen fixed top-0 left-0 z-[60] overflow-x-hidden overflow-y-auto bg-black bg-opacity-75`} aria-labelledby="modal-title" role="dialog" aria-modal="true">
+                <div className={`fixed inset-0 ${isShown ? 'opacity-100 scale-100' : 'opacity-0 scale-50'} transition-transform translate-y-fullduration-300 `} onClick={closeModal}></div>
+                <div className="fixed inset-0 z-10 overflow-y-auto" onClick={closeModal}>
+                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0  flex-col md:flex-row">
+                        <div className='relative' onClick={(e) => { e.stopPropagation(); }} >
+                            <button className='absolute text-white bg-none -right-2 -top-4' onClick={closeModal}><img src={CloseIcon} alt={'close-icon'} /></button>
+                            <div className={`transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg ${showFull ? 'scale-100' : 'scale-75 '}duration-300`}>
+                                <img src={previewImage} className={`${showFull ? 'hidden' : ''} w-full`} />
+                                <img src={fullImage} className={`${showFull ? '' : 'hidden'}`} onLoad={() => { setShowFull(true) }} />
                             </div>
                         </div>
                     </div>
-                </div> : <></>
-            }
+                </div>
+            </div>
         </>
     )
 }
